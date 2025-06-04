6c907f59d858fe7e9c65f0175fc3f6f83b3f1e00
Yogesh <kyogesh2301@gmail.com>
2025-06-04 23:21:01 +0530

    Setup for Render deployment

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -27,9 +27,9 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 SECRET_KEY = '+zy!9k=9pql5gz9bkqjore)k6r!%w0atk(@(!(!zvp5e(t2i8n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = [ '*'
     
 ]
 
@@ -148,4 +148,8 @@ EMAIL_HOST_PASSWORD = 'xyz' # host email password required
 # otherwise you will get SMTPAuthenticationError at /contactus
 # this process is required because google blocks apps authentication by default
 EMAIL_RECEIVING_USER = ['to@gmail.com'] # email on which you will receive messages sent from website
+
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
